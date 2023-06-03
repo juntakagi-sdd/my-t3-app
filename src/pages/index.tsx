e39604e8af4c152af5a2c203f9b6fcd6c7323710
juntakagi-sdd <juntakagisdd@gmail.com>
2023-06-03 22:35:27 +0900

    mod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,54 @@
-import { type Notes } from "@prisma/client";
 import { type NextPage } from "next";
-import { useState, useEffect, ChangeEvent } from "react";
-import Main from "~/components/Main";
-import Nav from "~/components/Nav";
+import { useState, ChangeEvent } from "react";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const [content, setContent] = useState("");
-  const [dataSource, setDataSource] = useState<Notes[]>([]);
-  const notes = api.note.fetch.useQuery();
+  const { isLoading, error, data, refetch } = api.note.fetch.useQuery();
   const createMutation = api.note.create.useMutation();
   const deleteMutation = api.note.delete.useMutation();
 
-  useEffect(() => {
-    if (notes.isFetched) {
-      setDataSource([...notes.data!]);
-    }
-  }, [notes.isFetched]);
+  if (error) {
+    return <>Error</>;
+  }
 
   const handleContentChange = (e: ChangeEvent<HTMLInputElement>) => {
     setContent(e.target.value);
   };
 
   const handleSaveClick = () => {
-    createMutation.mutate({ content: content });
+    createMutation.mutate({ content: content }, { onSuccess: () => refetch() });
     setContent("");
   };
 
   const handleDeleteClick = (id: number) => {
-    deleteMutation.mutate({ id: id });
+    deleteMutation.mutate({ id: id }, { onSuccess: () => refetch() });
   };
 
   return (
     <div>
-      <Nav />
-      <Main>
+      <nav className="bg-gray-800">
+        <div className="mx-auto px-4">
+          <div className="flex h-16 items-center">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-8 w-8"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                />
+              </div>
+            </div>
+            <div className="flex items-center">
+              <div className="ml-5 flex-shrink-0">
+                <span className="font-medium text-[hsl(280,100%,70%)]">
+                  T3 App
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <main>
         <div className="px-5 pt-5">
           <div className="grid grid-cols-3">
             <div className="col-span-2">
@@ -45,6 +59,7 @@ const Home: NextPage = () => {
                 className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-300"
                 value={content}
                 onChange={handleContentChange}
+                autoComplete="off"
               />
             </div>
             <div className="col-span-1 pl-3">
@@ -61,18 +76,18 @@ const Home: NextPage = () => {
             <table className="min-w-full leading-normal">
               <thead>
                 <tr>
-                  <th className="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold text-gray-600">
+                  <th className="w-40 border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold text-gray-600">
                     CreatedAt
                   </th>
-                  <th className="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold text-gray-600">
+                  <th className="w-40 border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold text-gray-600">
                     Content
                   </th>
                   <th className="border-b-2 bg-gray-100" />
                 </tr>
               </thead>
               <tbody>
-                {Object.keys(dataSource).length > 0 ? (
-                  dataSource.map((note) => (
+                {!isLoading ? (
+                  data!.map((note) => (
                     <tr key={note.id}>
                       <td className="border-b border-gray-200 bg-white px-5 py-3 text-sm">
                         <div>
@@ -106,7 +121,7 @@ const Home: NextPage = () => {
             </table>
           </div>
         </div>
-      </Main>
+      </main>
     </div>
   );
 };
--- src/server/api/routers/note.ts
+++ src/server/api/routers/note.ts
@@ -3,7 +3,11 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const noteRouter = createTRPCRouter({
   fetch: publicProcedure.query(async ({ ctx }) => {
-    const notes = await ctx.prisma.notes.findMany();
+    const notes = await ctx.prisma.notes.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
     return notes;
   }),
   create: publicProcedure
